6cb5ac06d4a112274e18493dfe5a9509e7e9b549
Terry Mace <terry.mace@payroc.com>
2021-09-22 14:12:42 +0100

    abstracted middleware implementation out into its own class rather than being implemented via an anonymous function

--- WebApplication/Middleware/AppBuilderExtensions.cs
+++ WebApplication/Middleware/AppBuilderExtensions.cs
@@ -7,14 +7,7 @@ namespace WebApplication.Middleware
     {
         public static void UseTestHeader(this IAppBuilder app)
         {
-            app.Use((context, next) =>
-            {
-                var testHeader = new KeyValuePair<string, string[]>("test-header", new string[] { "asdasdasd" });
-
-                context.Response.Headers.Add(testHeader);
-
-                return next();
-            });
+            app.Use<TestHeaderMiddleware>();
         }
     }
 }
---
+++ WebApplication/Middleware/TestHeaderMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApplication.Middleware
+{
+    public class TestHeaderMiddleware : OwinMiddleware
+    {
+        private readonly OwinMiddleware next;
+
+        public TestHeaderMiddleware(OwinMiddleware next) : base(next)
+        {
+            this.next = next;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            var testHeader = new KeyValuePair<string, string[]>("test-header", new string[] { "asdasdasd" });
+
+            context.Response.Headers.Add(testHeader);
+
+            return next.Invoke(context);
+        }
+    }
+}
