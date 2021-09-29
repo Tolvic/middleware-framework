f8c26fdc4c765cfb9041d718914cf053eb15b91c
Terry Mace <terry.mace@payroc.com>
2021-09-29 16:02:30 +0100

    Added unit test to demo how to unit test .NET Framework middleware

--- WebApplication.Tests/TestHeaderMiddlewareTests.cs
+++ WebApplication.Tests/TestHeaderMiddlewareTests.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using WebApplication.Middleware;
+using Xunit;
+using Microsoft.Owin;
+using Moq;
+using FluentAssertions;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebApplication.Tests
 {
     public class TestHeaderMiddlewareTests
     {
+        private readonly Mock<OwinMiddleware> _mockNextOwinMiddleware;
+
+        public TestHeaderMiddlewareTests()
+        {
+            _mockNextOwinMiddleware = new Mock<OwinMiddleware>((OwinMiddleware)null);
+
+            _mockNextOwinMiddleware.Setup(arg => arg.Invoke(It.IsAny<IOwinContext>())).Returns(Task.CompletedTask);
+
+        }
+
+        [Fact]
+        public async Task TestHeaderMiddleware_AddsHeaderToResponse()
+        {
+            // Arrange
+            var middlewareInstance = new TestHeaderMiddleware(_mockNextOwinMiddleware.Object);
+            var owinContext = GetMockOwinContext();
+            
+            // Act
+            await middlewareInstance.Invoke(owinContext);
+
+            // Assert
+            owinContext.Response.Headers.ContainsKey("test-header").Should().BeTrue();
+        }
+
+        private static IOwinContext GetMockOwinContext()
+        {
+            var requestMock = new Mock<IOwinRequest>();
+            requestMock.SetupGet(m => m.Scheme).Returns("https");
+            requestMock.SetupGet(m => m.Path).Returns(new PathString("/"));
+            requestMock.SetupGet(m => m.Method).Returns("GET");
+
+            var responseMock = new Mock<IOwinResponse>();
+            responseMock.SetupGet(m => m.Headers).Returns(new HeaderDictionary(new Dictionary<string, string[]>()));
+            
+            var owinContextMock = new Mock<IOwinContext>();
+            owinContextMock.SetupGet(m => m.Request).Returns(requestMock.Object);
+            owinContextMock.SetupGet(m => m.Response).Returns(responseMock.Object);
+
+            return owinContextMock.Object;
+        }
     }
 }
