69bd6ed5affb77356290c083349837aa349de4eb
Terry Mace <terry.mace@payroc.com>
2021-09-30 10:11:10 +0100

    Added an integration test

---
+++ WebApplication.Tests/TestHeaderMiddlewareIntegrationTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Owin.Testing;
+using Xunit;
+
+namespace WebApplication.Tests
+{
+    public class TestHeaderMiddlewareIntegrationTests
+    {
+        [Fact]
+        public async Task TestGoodMethod()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+
+                response.Headers.Contains("test-header").Should().BeTrue();
+            }
+        }
+    }
+}
